b9c7828c383bc5d19555532053d3c99afe21f13e
Joaquin Acosta <joaquinacosta397@outlook.com>
2024-01-03 15:03:08 -0300

    error

--- photo-gallery-app/src/app/gallery/page.tsx
+++ photo-gallery-app/src/app/gallery/page.tsx
@@ -9,15 +9,32 @@ const GalleryPage = () => {
   return (
     <section className="flex justify-between">
       <h1 className="text-4xl font-bold">Gallery</h1>
-      <Button asChild>
-        <CldUploadButton
-          onUpload={(results: CldUploadWidgetResults) => {
-            const res = results as UploadResult
-            const publicId = res.info.public_id
-            //   setImageId(publicId)
-          }}
-          uploadPreset="fioroasn"
-        />
+      <Button className="border" asChild>
+        <div className="flex gap-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5"
+            />
+          </svg>
+
+          <CldUploadButton
+            onUpload={(results: CldUploadWidgetResults) => {
+              const res = results as UploadResult
+              const publicId = res.info.public_id
+              //   setImageId(publicId)
+            }}
+            uploadPreset="fioroasn"
+          />
+        </div>
       </Button>
     </section>
   )
