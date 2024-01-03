e317951fbc3efba2f758b9aefc408adf33840c17
Joaquin Acosta <joaquinacosta397@outlook.com>
2024-01-03 20:31:34 -0300

    favorites gallery

--- photo-gallery-app/src/app/gallery/cloudinary-image.tsx
+++ photo-gallery-app/src/app/gallery/cloudinary-image.tsx
@@ -1,8 +1,41 @@
 'use client'
 
+import { HeartIcon } from '@/components/icons/heart'
 import { CldImage } from 'next-cloudinary'
-import React from 'react'
+import { setAsFavoriteAction } from './actions'
+import { useTransition } from 'react'
+import { SearchResult } from './page'
+import { FullHeartIcon } from '@/components/icons/fullheart'
 
-export default function CloudinaryImage(props: any) {
-  return <CldImage {...props} />
+export default function CloudinaryImage(
+  props: any & { imageData: SearchResult; path: string }
+) {
+  const [transition, startTransition] = useTransition()
+  const { imageData } = props
+  const isFavorited = imageData.tags?.includes('favorite')
+
+  return (
+    <div className="relative">
+      <CldImage {...props} src={imageData.public_id} />
+      {!isFavorited ? (
+        <HeartIcon
+          className="absolute top-2 right-2 hover:text-red-600 cursor-pointer"
+          onClick={() => {
+            startTransition(() => {
+              setAsFavoriteAction(imageData.public_id, false, props.path)
+            })
+          }}
+        />
+      ) : (
+        <FullHeartIcon
+          className="absolute top-2 right-2 hover:text-white text-red-500 cursor-pointer"
+          onClick={() => {
+            startTransition(() => {
+              setAsFavoriteAction(imageData.public_id, true, props.path)
+            })
+          }}
+        />
+      )}
+    </div>
+  )
 }
--- photo-gallery-app/src/app/layout.tsx
+++ photo-gallery-app/src/app/layout.tsx
@@ -4,6 +4,8 @@ import { Inter } from 'next/font/google'
 import { Button } from '@/components/ui/button'
 import './globals.css'
 import { cn } from '@/lib/utils'
+import { HeartIcon } from '@/components/icons/heart'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -22,24 +24,27 @@ function SideMenu() {
           </h2>
           <div className="space-y-1">
             <Button
-              variant="secondary"
+              variant="ghost"
               className="w-full justify-start flex gap-2"
+              asChild
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
-                />
-              </svg>
-              Gallery
+              <Link href={'/gallery'}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+                  />
+                </svg>
+                Gallery
+              </Link>
             </Button>
             <Button variant="ghost" className="w-full justify-start flex gap-2">
               <svg
@@ -58,22 +63,15 @@ function SideMenu() {
               </svg>
               Albums
             </Button>
-            <Button variant="ghost" className="w-full justify-start flex gap-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
-                />
-              </svg>
-              Favorites
+            <Button
+              variant="ghost"
+              className="w-full justify-start flex gap-2"
+              asChild
+            >
+              <Link href={'/favorites'}>
+                <HeartIcon />
+                Favorites
+              </Link>
             </Button>
           </div>
         </div>
