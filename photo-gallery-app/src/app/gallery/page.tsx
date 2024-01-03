bf9c0b11c1204118107af62ebabaa1e3a53a4655
Joaquin Acosta <joaquinacosta397@outlook.com>
2024-01-03 16:11:11 -0300

    gallery mapping

--- photo-gallery-app/src/app/gallery/page.tsx
+++ photo-gallery-app/src/app/gallery/page.tsx
@@ -1,22 +1,34 @@
-'use client'
-
+import { CldImage } from 'next-cloudinary'
 import UploadButton from './upload-button'
 import cloudinary from 'cloudinary'
+import CloudinaryImage from './cloudinary-image'
 
-const GalleryPage = async () => {
-  const result = await cloudinary.v2.search
+type SearchResult = {
+  public_id: string
+}
+
+export default async function GalleryPage() {
+  const results = (await cloudinary.v2.search
     .expression('resource_type:image')
     .sort_by('public_id', 'desc')
-    .max_results(30)
-    .execute()
-    .then((result) => console.log(result))
+    .max_results(5)
+    .execute()) as { resources: SearchResult[] }
 
   return (
     <section className="flex justify-between">
       <h1 className="text-4xl font-bold">Gallery</h1>
       <UploadButton />
+      <div className="grid grid-cols-4 gap-4">
+        {results.resources.map((result) => (
+          <CloudinaryImage
+            key={result.public_id}
+            src={result.public_id}
+            width="400"
+            height="300"
+            alt="an image of something"
+          />
+        ))}
+      </div>
     </section>
   )
 }
-
-export default GalleryPage
