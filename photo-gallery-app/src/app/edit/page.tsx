2d7c35cd2efe2ab5f97dfd107f0170f9230cd8ba
Joaquin Acosta <joaquinacosta397@outlook.com>
2024-01-04 20:43:28 -0300

    add transformations

--- photo-gallery-app/src/app/edit/page.tsx
+++ photo-gallery-app/src/app/edit/page.tsx
@@ -1,13 +1,22 @@
 'use client'
 
+import { Button } from '@/components/ui/button'
 import { CldImage } from 'next-cloudinary'
+import { useState } from 'react'
 
 interface Props {
   searchParams: { publicId: string }
 }
 
 const EditPage = ({ searchParams: { publicId } }: Props) => {
-  console.log(publicId)
+  const [transformation, setTransformation] = useState<
+    | undefined
+    | 'generative-fill'
+    | 'blur'
+    | 'grayscale'
+    | 'pixelate'
+    | 'removeBg'
+  >(undefined)
 
   return (
     <section>
@@ -16,7 +25,82 @@ const EditPage = ({ searchParams: { publicId } }: Props) => {
           <h1 className="text-4xl font-bold">Edit {publicId}</h1>
         </div>
 
-        <CldImage src={publicId} width={300} height={200} alt="text" />
+        <div className="flex gap-4">
+          <Button
+            variant={'ghost'}
+            onClick={() => setTransformation(undefined)}
+          >
+            Clear All
+          </Button>
+          <Button onClick={() => setTransformation('generative-fill')}>
+            Apply Generate Fill
+          </Button>
+          <Button onClick={() => setTransformation('blur')}>Apply Blur</Button>
+          <Button onClick={() => setTransformation('grayscale')}>
+            Conver to Gray
+          </Button>
+          <Button onClick={() => setTransformation('pixelate')}>
+            Pixelate
+          </Button>
+          <Button onClick={() => setTransformation('removeBg')}>
+            Remove Background
+          </Button>
+        </div>
+
+        <div className="grid grid-cols-2 gap-12">
+          <CldImage src={publicId} width={1200} height={1400} alt="text" />
+
+          {transformation === 'generative-fill' && (
+            <CldImage
+              src={publicId}
+              width={1200}
+              height={1400}
+              alt="text"
+              crop="pad"
+              fillBackground
+            />
+          )}
+
+          {transformation === 'blur' && (
+            <CldImage
+              src={publicId}
+              width={1200}
+              height={1400}
+              blur="800"
+              alt="text"
+            />
+          )}
+
+          {transformation === 'grayscale' && (
+            <CldImage
+              src={publicId}
+              width={1200}
+              height={1400}
+              grayscale
+              alt="text"
+            />
+          )}
+
+          {transformation === 'pixelate' && (
+            <CldImage
+              src={publicId}
+              width={1200}
+              height={1400}
+              pixelate
+              alt="text"
+            />
+          )}
+
+          {transformation === 'removeBg' && (
+            <CldImage
+              src={publicId}
+              width={1200}
+              height={1400}
+              removeBackground
+              alt="text"
+            />
+          )}
+        </div>
       </div>
     </section>
   )
